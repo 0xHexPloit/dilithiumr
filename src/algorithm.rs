5a9cb40a8d3e9ef12632b780ca8ad0b3627f1ae7
Hugo PEYRON <hugo.peyron@proton.me>
2023-08-07 12:08:44 +0200

    Modifying the way how to construct Dilithium instances

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -1,5 +1,30 @@
-use crate::constants::DilithiumParams;
+pub struct Dilithium<
+    const K: u8,
+    const L: u8,
+    const SIGNATURE_KEY_SIZE: usize,
+    const PUBLIC_KEY_SIZE: usize
+>;
 
-pub struct Dilithium {
-    pub (crate) params: DilithiumParams
+
+impl <
+    const K: u8,
+    const L: u8,
+    const SIGNATURE_KEY_SIZE: usize,
+    const PUBLIC_KEY_SIZE: usize
+>Dilithium<
+    K,
+    L,
+    SIGNATURE_KEY_SIZE,
+    PUBLIC_KEY_SIZE
+> {
+
+    pub fn keygen(&self) -> ([u8; SIGNATURE_KEY_SIZE], [u8; PUBLIC_KEY_SIZE]) {
+
+
+
+        let signature_key = [0u8; SIGNATURE_KEY_SIZE];
+        let public_key = [0u8; PUBLIC_KEY_SIZE];
+
+        (signature_key, public_key)
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,27 @@ use algorithm::Dilithium;
 
 mod constants;
 mod algorithm;
+mod helper;
 
 
-pub const DILITHIUM2: Dilithium = Dilithium {params: constants::dilithium2::PARAMS};
-pub const DILITHIUM3: Dilithium = Dilithium {params: constants::dilithium3::PARAMS};
-pub const DILITHIUM5: Dilithium = Dilithium {params: constants::dilithium5::PARAMS};
+pub const DILITHIUM2: Dilithium<
+    4,
+    4,
+    2420,
+    1312
+> = Dilithium;
+
+pub const DILITHIUM3: Dilithium<
+    6,
+    5,
+    3293,
+    1952
+> = Dilithium;
+
+
+pub const DILITHIUM5: Dilithium<
+    8,
+    7,
+    4595,
+    2592
+> = Dilithium;
