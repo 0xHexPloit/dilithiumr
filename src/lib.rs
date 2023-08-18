948b85aec31b3ffd2f7f3b112564f1fd6a31a205
Hugo PEYRON <hugo.peyron@proton.me>
2023-08-18 16:32:15 +0200

    Adding wasm support

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use crate::constants::Q;
+wai_bindgen_rust::export!("universal_dilithium.wai");
+
+use crate::constants::{Q, ZETA_BYTES};
 use algorithm::Dilithium;
 
 mod algebra;
@@ -11,7 +13,7 @@ mod reduce;
 mod rejection;
 mod rounding;
 
-pub const DILITHIUM2: Dilithium<
+const DILITHIUM2: Dilithium<
     4,
     4,
     2,
@@ -27,7 +29,7 @@ pub const DILITHIUM2: Dilithium<
     2420,
 > = Dilithium;
 
-pub const DILITHIUM3: Dilithium<
+const DILITHIUM3: Dilithium<
     6,
     5,
     4,
@@ -42,7 +44,7 @@ pub const DILITHIUM3: Dilithium<
     1952,
     3293,
 > = Dilithium;
-pub const DILITHIUM5: Dilithium<
+const DILITHIUM5: Dilithium<
     8,
     7,
     2,
@@ -57,3 +59,82 @@ pub const DILITHIUM5: Dilithium<
     2592,
     4595,
 > = Dilithium;
+
+
+pub struct UniversalDilithium;
+
+impl UniversalDilithium {
+    fn check_seed(seed: Vec<u8>) -> [u8; ZETA_BYTES] {
+        if seed.len() != ZETA_BYTES {
+            panic!("Invalid seed length");
+        }
+
+        let mut custom_seed = [0u8; ZETA_BYTES];
+        custom_seed.copy_from_slice(&seed);
+        custom_seed
+    }
+}
+
+impl universal_dilithium::UniversalDilithium for UniversalDilithium {
+    fn dilithium_mode2_keygen(seed: Option<Vec<u8>>) -> (Vec<u8>, Vec<u8>) {
+        if let Some(seed) = seed {
+            let custom_seed = UniversalDilithium::check_seed(seed);
+            let (sk, pk) = DILITHIUM2.keygen(Some(&custom_seed));
+            (sk.to_vec(), pk.to_vec())
+        } else {
+            let (sk, pk) = DILITHIUM2.keygen(None);
+            (sk.to_vec(), pk.to_vec())
+        }
+    }
+    fn dilithium_mode3_keygen(seed: Option<Vec<u8>>) -> (Vec<u8>, Vec<u8>) {
+        if let Some(seed) = seed {
+            if seed.len() != ZETA_BYTES {
+                panic!("Invalid seed length");
+            }
+            let custom_seed = UniversalDilithium::check_seed(seed);
+            let (sk, pk) = DILITHIUM3.keygen(Some(&custom_seed));
+            (sk.to_vec(), pk.to_vec())
+        } else {
+            let (sk, pk) = DILITHIUM3.keygen(None);
+            (sk.to_vec(), pk.to_vec())
+        }
+    }
+
+    fn dilithium_mode5_keygen(seed: Option<Vec<u8>>) -> (Vec<u8>, Vec<u8>) {
+        if let Some(seed) = seed {
+            if seed.len() != ZETA_BYTES {
+                panic!("Invalid seed length");
+            }
+            let custom_seed = UniversalDilithium::check_seed(seed);
+            let (sk, pk) = DILITHIUM5.keygen(Some(&custom_seed));
+            (sk.to_vec(), pk.to_vec())
+        } else {
+            let (sk, pk) = DILITHIUM5.keygen(None);
+            (sk.to_vec(), pk.to_vec())
+        }
+    }
+
+    fn dilithium_mode2_sign(key: Vec<u8>, message: Vec<u8>, rsigning: bool) -> Vec<u8> {
+        DILITHIUM2.sign(&key, &message, rsigning).to_vec()
+    }
+
+    fn dilithium_mode3_sign(key: Vec<u8>, message: Vec<u8>, rsigning: bool) -> Vec<u8> {
+        DILITHIUM3.sign(&key, &message, rsigning).to_vec()
+    }
+
+    fn dilithium_mode5_sign(key: Vec<u8>, message: Vec<u8>, rsigning: bool) -> Vec<u8> {
+        DILITHIUM5.sign(&key, &message, rsigning).to_vec()
+    }
+
+    fn dilithium_mode2_verify(key: Vec<u8>, message: Vec<u8>, signature: Vec<u8>) -> bool {
+        DILITHIUM2.verify(&key, &message, &signature)
+    }
+
+    fn dilithium_mode3_verify(key: Vec<u8>, message: Vec<u8>, signature: Vec<u8>) -> bool {
+        DILITHIUM3.verify(&key, &message, &signature)
+    }
+
+    fn dilithium_mode5_verify(key: Vec<u8>, message: Vec<u8>, signature: Vec<u8>) -> bool {
+        DILITHIUM5.verify(&key, &message, &signature)
+    }
+}
